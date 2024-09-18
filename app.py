167833803eb5db465306fa48f4328dca3fa0f3ff
nikhil <nb9690@gmail.com>
2024-09-18 22:10:17 +0530

    secret key added

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 sitemapper = Sitemapper(app=app) # Create and initialize the sitemapper
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-app.secret_key = secret_key
+app.secret_key =  os.urandom(16).hex()
 
 
 class User(db.Model):
